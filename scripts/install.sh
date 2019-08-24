9b3ffaae1b0a8558f642bcb1c53846f144c0823c
nerdeveloper <odirionye@gmail.com>
2019-08-24 20:20:59 +0100

    uncomment devops pack  tools

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,55 +26,55 @@ sudo apt-get install python3 -y && sudo apt-get install python3-pip -y
 echo -e " ${PURPLE} Python and Pip has been installed! \e[0m "
 
 
-# # Installation for AWS CLI
-# echo -e " ${BLUE} Installing AWS CLI \e[0m "
-# sleep 2
-# sudo apt-get install awscli -y
-# echo -e " ${BLUE} AWS CLI has been installed! \e[0m "
+# Installation for AWS CLI
+echo -e " ${BLUE} Installing AWS CLI \e[0m "
+sleep 2
+sudo apt-get install awscli -y
+echo -e " ${BLUE} AWS CLI has been installed! \e[0m "
 
 
-# #Installations for Google Clouc SDK
-# echo -e " ${BLUE} Installing Google Cloud SDK"
-# sleep 2
+#Installations for Google Clouc SDK
+echo -e " ${BLUE} Installing Google Cloud SDK"
+sleep 2
 
-# # Create an environment variable for the correct distribution
-# export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
-# # Add the Cloud SDK distribution URI as a package source
-# echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-# # Import the Google Cloud Platform public key
-# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-# # Update the package list and install the Cloud SDK
-# sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
+# Update the package list and install the Cloud SDK
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
 
-# echo -e " ${BLUE} Google Cloud CLI has been installed! \e[0m "
+echo -e " ${BLUE} Google Cloud CLI has been installed! \e[0m "
 
-# #Installations for Microsft Azure CLI
-# echo -e " ${BLUE} Installing Microsoft Azure CLI"
-# sleep 2
+#Installations for Microsft Azure CLI
+echo -e " ${BLUE} Installing Microsoft Azure CLI"
+sleep 2
 
-# # Get packages needed for the install process:
-# sudo apt-get update -y
-# sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
+# Get packages needed for the install process:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
 
-# # Download and install the Microsoft signing key:
-# curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
-# gpg --dearmor | \
-# sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+# Download and install the Microsoft signing key:
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+gpg --dearmor | \
+sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 
 
-# # Add the Azure CLI software repository
-# AZ_REPO=$(lsb_release -cs)
-# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-# sudo tee /etc/apt/sources.list.d/azure-cli.list
+# Add the Azure CLI software repository
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-# # Update repository information and install the azure-cli package:
-# sudo apt-get update
-# sudo apt-get install azure-cli -y
+# Update repository information and install the azure-cli package:
+sudo apt-get update
+sudo apt-get install azure-cli -y
 
-# echo -e " ${BLUE} Microsoft Azure CLI has been installed! \e[0m "
+echo -e " ${BLUE} Microsoft Azure CLI has been installed! \e[0m "
 
 
 #Installations for Go
@@ -93,75 +93,75 @@ source /home/vagrant/.bashrc
 echo -e " ${BLUE} Go has been installed! \e[0m"
 
 
-# #Installations for Terraform
-# echo -e " ${BLUE} Installing Terraform"
-# sleep 2
-# cd /tmp && \
-# wget https://releases.hashicorp.com/terraform/${TERRAFROM_VERSION}/terraform_${TERRAFROM_VERSION}_linux_amd64.zip  && \
-# unzip terraform_${TERRAFROM_VERSION}_linux_amd64.zip && \
-# mv terraform /usr/bin
-# echo -e " ${BLUE} Terraform has been installed! \e[0m"
+#Installations for Terraform
+echo -e " ${BLUE} Installing Terraform"
+sleep 2
+cd /tmp && \
+wget https://releases.hashicorp.com/terraform/${TERRAFROM_VERSION}/terraform_${TERRAFROM_VERSION}_linux_amd64.zip  && \
+unzip terraform_${TERRAFROM_VERSION}_linux_amd64.zip && \
+mv terraform /usr/bin
+echo -e " ${BLUE} Terraform has been installed! \e[0m"
 
-# #Installations for Ansible
-# echo -e " ${BLUE} Installing Ansible"
-# sleep 2
-# sudo apt-get update -y
-# sudo apt-get install software-properties-common -y
-# sudo apt-add-repository ppa:ansible/ansible -y  && \
-# sudo apt-get update -y && \
-# sudo apt-get install ansible -y
+#Installations for Ansible
+echo -e " ${BLUE} Installing Ansible"
+sleep 2
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y  && \
+sudo apt-get update -y && \
+sudo apt-get install ansible -y
 
-# echo -e " ${BLUE} Ansible has been installed! \e[0m"
-
-
-# #Installations for Dotnetcore
-# echo -e " ${BLUE} Installing Dotnetcore"
-# sleep 2
-# wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
-
-# sudo apt-get install apt-transport-https
-# sudo apt-get update -y 
-# sudo apt-get install dotnet-sdk-2.2 -y 
-# echo -e " ${BLUE} Dotnetcore has been installed! \e[0m"
+echo -e " ${BLUE} Ansible has been installed! \e[0m"
 
 
-# #Installations for Docker
-# echo -e " ${BLUE} Installing DOCKER"
-# sleep 2
-# usermod -G docker ubuntu
-# sudo apt-get install \
-# apt-transport-https \
-# ca-certificates \
-# curl \
-# gnupg-agent \
-# software-properties-common -y
+#Installations for Dotnetcore
+echo -e " ${BLUE} Installing Dotnetcore"
+sleep 2
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+sudo apt-get update -y 
+sudo apt-get install dotnet-sdk-2.2 -y 
+echo -e " ${BLUE} Dotnetcore has been installed! \e[0m"
 
-# sudo apt-key fingerprint 0EBFCD88
 
-# sudo add-apt-repository \
-# "deb [arch=amd64] https://download.docker.com/linux/ubuntu
-#     $(lsb_release -cs) \
-# stable"
-# sudo apt-get update
-# sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+#Installations for Docker
+echo -e " ${BLUE} Installing DOCKER"
+sleep 2
+usermod -G docker ubuntu
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common -y
 
-# echo -e " ${BLUE} Docker has been installed! \e[0m"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# #Installations for Kubernetes(Kops)
-# echo -e " ${BLUE} Installing Kubernetes(Kops)"
-# sleep 2
-# curl -LO https://github.com/kubernetes/kops/releases/download/${KOPS_VERSION}/kops-linux-amd64
-# chmod +x kops-linux-amd64
-# sudo mv ./kops-linux-amd64 /usr/bin/kops
-# echo -e " ${BLUE} Kubernetes(Kops)has been installed! \e[0m"
+sudo apt-key fingerprint 0EBFCD88
 
-# #Installations for Nodejs
-# echo -e " ${BLUE} Installing Nodejs"
-# sleep 2
-# curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION} -o nodesource_setup.sh && \
-# sudo bash nodesource_setup.sh && \
-# sudo apt-get install nodejs -y
-# echo -e " ${BLUE} Nodejs has been installed! \e[0m"
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu
+    $(lsb_release -cs) \
+stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo -e " ${BLUE} Docker has been installed! \e[0m"
+
+#Installations for Kubernetes(Kops)
+echo -e " ${BLUE} Installing Kubernetes(Kops)"
+sleep 2
+curl -LO https://github.com/kubernetes/kops/releases/download/${KOPS_VERSION}/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv ./kops-linux-amd64 /usr/bin/kops
+echo -e " ${BLUE} Kubernetes(Kops)has been installed! \e[0m"
+
+#Installations for Nodejs
+echo -e " ${BLUE} Installing Nodejs"
+sleep 2
+curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION} -o nodesource_setup.sh && \
+sudo bash nodesource_setup.sh && \
+sudo apt-get install nodejs -y
+echo -e " ${BLUE} Nodejs has been installed! \e[0m"
